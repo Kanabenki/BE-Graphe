42db886fc0b806cff54b21564036adf5784f1d8a
jujudu121212 <julienferry12@gmail.com>
2018-05-23 19:06:53 +0200

    added static main method and class to run performance tests

---
+++ src/test/org/insa/algo/shortestpath/runPerformanceTests.java
@@ -0,0 +1,19 @@
+package org.insa.algo.shortestpath;
+
+import java.io.IOException;
+
+public class runPerformanceTests {
+   static int taille = 100;
+   public static void main(String[] args) {
+      try {
+         System.out.println("**** Running test for size = " + taille + " ****");
+         PerformanceAlgorithmTest perfTest = new PerformanceAlgorithmTest(taille);
+         perfTest.runTest();
+         System.out.println("**** End of test ****");
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      
+   }
+}
